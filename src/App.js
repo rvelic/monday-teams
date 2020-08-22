8b71c73c4f33331fd104bcd370997247b09be91f
Roman Velic <roman.velic@gmail.com>
2020-08-22 18:51:56 +0200

    Extract teams
    Get teams with people from current items and display them in a simple way. teamsColumn settings defines which column will be used for scraping teams

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import mondaySdk from "monday-sdk-js";
-// eslint-disable-next-line
 const monday = mondaySdk();
 
 class App extends React.Component {
@@ -11,17 +10,83 @@ class App extends React.Component {
     // Default state
     this.state = {
       settings: {},
-      name: "",
+      context: {},
+      itemIds: [],
+      items: [],
+      teamIds: [],
+      teams: [],
+      name: ""
     };
   }
 
   componentDidMount() {
+    monday.listen('settings', (res) => {
+      this.setState({settings: res.data});
+    })
 
-    // TODO: set up event listeners
+    monday.listen('context', (res) => {
+      this.setState({context: res.data});
+    })
+
+    monday.listen('itemIds', (res) => {
+      this.setState({itemIds: res.data});
+
+      if (!this.state.settings.teamsColumn) {
+        console.log('TeamsColumn is not set!')
+        // TODO: add validation
+      }
+      const teamsColumn = Object.keys(this.state.settings.teamsColumn).shift();
+
+      monday.api(`query {      
+        boards(ids:[${this.state.context.boardId}]) {
+          name
+          items(ids:[${this.state.itemIds}]) {
+            name
+            column_values(ids:[${teamsColumn}]) {
+              id
+              value
+            }
+          }
+        }
+      }`).then((res) => {
+        this.setState({items: res.data.boards[0].items});
+        this.setState({teamIds: this.state.items.reduce((teamIds, item) => {
+          item.column_values
+            .filter(col => col.id === teamsColumn && col.value)
+            .map(col => {
+              JSON.parse(col.value).personsAndTeams
+              .forEach(personOrTeam => {
+                if (personOrTeam.kind === 'team' && !teamIds.includes(personOrTeam.id))
+                  teamIds.push(personOrTeam.id);
+              })
+            })
+          return teamIds;
+        }, [])});
+        
+        return monday.api(`query {
+          teams(ids:[${this.state.teamIds}]) {
+            id
+            name
+            users {
+              id
+              name
+              time_zone_identifier
+            }
+          }
+        }`);
+      }).then((res) => {
+        this.setState({teams: res.data.teams});
+      })
+    })
   }
 
   render() {
-    return <div className="App">Hello, Apps!</div>;
+    return <div className="App">
+      {this.state.teams.map((team) => {
+        return <div><p>{team.id}</p>
+        <p>{team.name}</p></div>
+      })}
+    </div>;
   }
 }
 
